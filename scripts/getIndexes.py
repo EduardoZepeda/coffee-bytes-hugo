ebd566bebd0f81ef4289ec869370b8635e08c204
Eduardo Zepeda <eduardozepeda@coffeebytes.dev>
2024-03-24 20:41:59 -0600

    update getIndexes script, add frontmatter aliases update

--- scripts/getIndexes.py
+++ scripts/getIndexes.py
@@ -22,6 +22,17 @@ def get_unstranslated_indexes():
     return mdIndexFiles
 
 
+def get_all_original_indexes():
+    mdIndexFiles = []
+    for dirpath, subdirs, files in os.walk(
+        os.path.join(os.getcwd(), "content", "posts")
+    ):
+        for x in files:
+            if x.endswith("index.md"):
+                mdIndexFiles.append(os.path.join(dirpath, x))
+    return mdIndexFiles
+
+
 def get_translated_indexes():
     mdIndexFiles = []
     for dirpath, subdirs, files in os.walk(
@@ -53,38 +64,56 @@ def get_slugged_titles(list):
     return titles
 
 
-def read_markdown(list):
-    slugged_titles = get_slugged_titles(list)
-    for file in list:
-        with open(file, "r") as f:
-            content = f.read()
-            links = re.findall("\]\(/(.*?)\)", content)
-            for link in links:
-                highest = [
-                    {
-                        "score": similar(link, slugged_title),
-                        "link": link,
-                        "slugged_title": slugged_title,
-                    }
-                    for slugged_title in slugged_titles
-                ]
-                highest.sort(key=lambda x: x["score"], reverse=True)
-                highest_coincidence = highest[0]
-                content = content.replace(
-                    highest_coincidence["link"],
-                    "blog/" + highest_coincidence["slugged_title"] + "/",
-                )
+# def read_markdown(list):
+#     slugged_titles = get_slugged_titles(list)
+#     for file in list:
+#         with open(file, "r") as f:
+#             content = f.read()
+#             links = re.findall("\]\(/(.*?)\)", content)
+#             for link in links:
+#                 highest = [
+#                     {
+#                         "score": similar(link, slugged_title),
+#                         "link": link,
+#                         "slugged_title": slugged_title,
+#                     }
+#                     for slugged_title in slugged_titles
+#                 ]
+#                 highest.sort(key=lambda x: x["score"], reverse=True)
+#                 highest_coincidence = highest[0]
+#                 content = content.replace(
+#                     highest_coincidence["link"],
+#                     "blog/" + highest_coincidence["slugged_title"] + "/",
+#                 )
 
+#         with open(file, "w") as f:
+#             f.write(content)
+
+
+def add_aliases_to_markdown_file(markdown_file, aliases):
+    post = frontmatter.load(markdown_file)
+    aliases.append("/" + slugify(post["title"])) if "/" + slugify(
+        post["title"]
+    ) not in aliases else aliases
+    if "url" in post.keys():
+        aliases.append("/" + post["url"]) if "/" + post[
+            "url"
+        ] not in aliases else aliases
+    post["aliases"] = aliases
+    return frontmatter.dumps(post)
+
+
+def add_aliases_to_indexes(indexes):
+    for file in indexes:
+        markdown_with_aliases = add_aliases_to_markdown_file(
+            file, ["/" + os.path.basename(os.path.dirname(file))]
+        )
         with open(file, "w") as f:
-            f.write(content)
+            f.write(markdown_with_aliases)
 
 
 def main():
-    # for index in get_indexes():
-    #     sys.stdout.write(index + "\n")
-    # read_markdown(get_translated_indexes())
-    # read_markdown(get_translated_indexes())
-    translate_indexes(get_unstranslated_indexes())
+    add_aliases_to_indexes(get_all_original_indexes())
 
 
 if __name__ == "__main__":
